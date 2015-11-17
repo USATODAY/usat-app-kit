a5bf355993e1a8fa28881795ebc3945e1cdd3209
Mitchell Thorson <mitch.thorson@gmail.com>
2015-11-17 14:47:54 -0500

    fix data watch source

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -9,7 +9,7 @@ var watch    = require('gulp-watch');
 gulp.task('watch', ['browserSync'], function() {
   watch(images.src, function() { gulp.start('images'); });
   watch(sass.src, function() { gulp.start('sass'); });
-  watch(data.src, function() { gulp.start('html'); gulp.start('data'); });
+  watch(data.src + '*', function() { gulp.start('html'); gulp.start('data'); });
   watch(fonts.src, function() { gulp.start('fonts'); });
   watch(html.watch, function() { gulp.start('html'); });
 });
