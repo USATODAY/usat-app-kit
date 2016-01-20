4485f470cdf0bbf0cb74288ef652bc5acabeceda
mitchthorson <mthorson@usatoday.com>
2016-01-20 15:35:16 -0500

    update bootstrap script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -9,6 +9,12 @@ SLUG=${PWD##*/}
 # set up graphic config based on folder name as slug
 node bin/setup.js
 
+# remove existing git stuff
+rm -rf .git
+
+# initialize as a fresh repo
+git init
+
 # commit new graphic
 git add .
 git commit -m "setup $SLUG from template."
