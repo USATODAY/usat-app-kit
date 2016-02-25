3f2807601f4ad45f56a776e0ab50f01bee7ff91f
mitchthorson <mitch.thorson@gmail.com>
2016-02-25 15:08:22 -0500

    update analytics library

--- app/js/lib/analytics.js
+++ app/js/lib/analytics.js
@@ -7,6 +7,24 @@
         root.Analytics = factory(_);
   }
 }(this, function(_) {
+
+    function getScript(src, callback) {
+        var s = document.createElement('script');
+        s.src = src;
+        s.async = true;
+        s.onreadystatechange = s.onload = function() {
+            if (!callback.done && (!s.readyState || /loaded|complete/.test(s.readyState))) {
+                callback.done = true;
+                callback();
+            }
+        };
+        document.querySelector('head').appendChild(s);
+    }
+
+    //array to hold queued analytics calls while library loads
+    var queue = [];
+    var isLoading = false;
+
     var Analytics = {
 
         isInitialized: false,
@@ -17,7 +35,9 @@
             clicknameBase: "",
             eventtype: "uotrack",
             linkTrackEvents: "none",
-            linkTrackVars: "prop41"
+            linkTrackVars: "prop41,prop64,prop16",
+            contenttype: 'interactives',
+            clickPage: location.href
         },
         
         pageViewArgs: {
@@ -39,41 +59,58 @@
         },
      
         click: function(eventName, options) {
-            if (window.utag) {
+            eventName = eventName.replace(/ /g, '-');
+            
+            if (options) {
+                options = _.extend(this.clickArgs, options);
+            } else if (eventName) {
+                // replace event name spaces with '-'
                 eventName = eventName.replace(/ /g, '-');
-                if (options) {
-                    options = _.extend(this.clickArgs, options);
-                }
-                else if (eventName) {
-                    // replace event name spaces with '-'
-                    eventName = eventName.replace(/ /g, '-');
-                    options = _.extend(this.clickArgs, {
-                        clickName: this.clickArgs.clicknameBase + eventName
-                    });
-                }
-                else {
-                    options = this.clickArgs;
-                }
-                // console.log('link', this.clickArgs, options);
+                options = _.extend(this.clickArgs, {
+                    clickName: this.clickArgs.clicknameBase + eventName
+                });
+            } else {
+                options = this.clickArgs;
+            }
+
+            if (window.utag) {
                 window.utag.track('link', options);
             }
             else {
-                this.loadUtag();
+                //queue event
+                queue.push([eventName, options]);
+                if (!isLoading) {
+                    this.loadUtag();
+                }
             }
 
         },
      
         // load utag script, loading with require causes errors, utag requires utag_data
         loadUtag: function() {
+            isLoading = true;
             window.utag_data = _.extend(this.pageViewArgs, {
                 partner_type: 'basic'
             });
             var self = this,
                 useSSL = 'https:' === document.location.protocol,
-                src = (useSSL ? 'https:' : 'http:') + '//tealium.hs.llnwd.net/o43/utag/gci/usat/prod/utag.js',
-                script = document.createElement('script');
-            script.src = src;
-            document.getElementsByTagName('head')[0].appendChild(script);
+                // src = (useSSL ? 'https:' : 'http:') + '//tealium.hs.llnwd.net/o43/utag/gci/usat/prod/utag.js',
+                src = "//tags.tiqcdn.com/utag/gci/usat/prod/utag.js";
+                getScript(src, function() {
+                    isLoading = false;
+
+                    //loop through click queue
+                    _.each(queue, function(click) {
+                        self.click(click[0], click[1]);
+                    });
+
+                    //reset queue
+                    queue = [];
+                });
+            //     script = document.createElement('script');
+            // script.src = src;
+            // document.getElementsByTagName('head')[0].appendChild(script);
+            // _.delay(function() {console.log(window.utag)}, 100);
 
         },
      
@@ -106,13 +143,18 @@
                 this.loadUtag();
             }
         },
-        setup: function(clicknameBase) {
+        setup: function(clicknameBase, opts) {
+            opts = opts || {};
             if (!this.isInitialized) {
                 this.setClickArgs({
-                    'clicknameBase': clicknameBase
+                    'clicknameBase': 'usat-interactive-' + clicknameBase + '-'
                 });
 
+                this.setPageViewArgs(opts);
+
                 this.loadUtag();
+
+                this.isInitialized = true;
             }
         }
     };
